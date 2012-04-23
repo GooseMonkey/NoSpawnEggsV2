b0a96a3aa523913e76320392162d5af3d35b5b61
Joshua <goosemonkey97@gmail.com>
2012-04-22 20:53:04 -0700

    #5: Begin work on plugin version

--- NoSpawnEggsV2/src/me/goosemonkey/NoSpawnEggsV2/NoSpawnEggs.java
+++ NoSpawnEggsV2/src/me/goosemonkey/NoSpawnEggsV2/NoSpawnEggs.java
@@ -4,5 +4,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoSpawnEggs extends JavaPlugin
 {
+	@Override
+	public void onEnable()
+	{
+		
+	}
 	
+	/**
+	 * Gets the current version of the plugin, change this to distribute different versions
+	 * @return Full, Moderate, or Light
+	 */
+	public static PluginVersion getVersion()
+	{
+		return PluginVersion.FULL;
+	}
+	
+	public enum PluginVersion
+	{
+		FULL, MODERATE, LIGHT
+	}
 }
